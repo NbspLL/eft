6d39d1e482043acb2aabf06d569e8321672e6992
jial <jial822@outlook.com>
2022-09-17 19:07:40 +0800

    INIT

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <button>可接单点我</button>
+    <h1>张三打手：不在线</h1>
+    <!-- <h1>李四打手：在线</h1>
+    <h1>王五打手：在线</h1>
+    <h1>吴六打手：接单中</h1> -->
+
+    <!-- <h1>张三打手：人狠话不多<button>下单</button></h1>
+    <h1>李四打手：人猛抢刚<button>下单</button></h1>
+    <h1>王五打手：猥琐发育<button>下单</button></h1>
+    <h1>吴六打手：话痨 肤白长腿 大圆脸<button>下单</button> </h1> -->
+  </div>
+</template>
